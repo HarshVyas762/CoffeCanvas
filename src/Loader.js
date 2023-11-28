54ca258bcaba1508f7a9bc5a2d8101ef25812258
Harsh Vyas <harshvyas@C02G80A9Q6L5.local>
2023-11-28 13:00:49 +0530

    update

---
+++ src/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './loader.css'
+
+function Loader() {
+    return (
+        <div style={{background:'#f1c40f', height:'100%', width:'100%'}}>
+            <div class="loader_cup">
+                <div class="h"></div>
+            </div>
+        </div>
+    )
+}
+export default Loader
