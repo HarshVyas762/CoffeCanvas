c5a8eb19daa5bc147cf04de1d41b6689c4ba4196
Harsh Vyas <harshvyas@C02G80A9Q6L5.local>
2023-12-18 12:25:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -104,6 +104,7 @@ function App() {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
         <link href="https://fonts.cdnfonts.com/css/raleway-5" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'></link>
         <meta charset="utf-8" />
         <style>
           @import url('https://fonts.cdnfonts.com/css/red-hat-display');
